1fadb915bdcad6340d276504a98f7c18343065a2
NinoDui <xuyue.yolanda@gmail.com>
2023-09-27 22:06:11 +0800

    [INIT] Initialize the project; [FEAT] Export highlightes and notes for a certain book to csv file

---
+++ notexport/__init__.py
@@ -0,0 +1,1 @@
+from .runner import fetch_notes, load_query
---
+++ tests/test_runner.py
@@ -0,0 +1,19 @@
+import unittest
+
+from notexport import fetch_notes, load_query
+
+
+class RunnerTest(unittest.TestCase):
+    def test_load_query(self):
+        query_path = "sql/ibook_query_book_by_name.sql"
+        query = load_query(query_path, kw_title="Tomorrow is another day")
+        self.assertIn("Tomorrow is", query)
+
+    def test_query_notes(self):
+        kwargs = {"kw_title": "Growth Mindset"}
+        result = fetch_notes(**kwargs)
+        self.assertGreater(result.shape[0], 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
